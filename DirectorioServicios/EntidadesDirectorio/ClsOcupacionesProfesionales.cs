4e8f900d404e9d5e60551246ab208cf034efb4e4
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-06 12:16:52 -0600

    CLSOcupaciones

---
+++ DirectorioServicios/EntidadesDirectorio/ClsOcupacionesProfesionales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDirectorio
+{
+    public class ClsOcupacionesProfesionales
+    {
+        int vgn_ID_Usuario, vgn_ID_Ocupacion;
+
+        public ClsOcupacionesProfesionales()
+        {
+            vgn_ID_Ocupacion = 0;
+            vgn_ID_Usuario = 0;
+        }
+
+        public ClsOcupacionesProfesionales(int vgn_ID_Usuario, int vgn_ID_Ocupacion)
+        {
+            this.vgn_ID_Usuario = vgn_ID_Usuario;
+            this.vgn_ID_Ocupacion = vgn_ID_Ocupacion;
+        }
+
+        public int ID_Usuario { get => vgn_ID_Usuario; set => vgn_ID_Usuario = value; }
+        public int ID_Ocupacion { get => vgn_ID_Ocupacion; set => vgn_ID_Ocupacion = value; }
+    }
+}
