47415a6a83cf4a025820ea6d83f8cc1d2be39f0d
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-06 11:22:29 -0600

    Varios

--- DirectorioServicios/EntidadesDirectorio/ClsUsuarios.cs
+++ DirectorioServicios/EntidadesDirectorio/ClsUsuarios.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntidadesDirectorio
 {
-    public class Class1
+    public class ClsUsuarios
     {
+        //ARGUMENTOS
+        int ID_Usuario, Calif_Contador, Calif_Suma;
+        string Nombre_Profesional, Apellido1_Profesional, Apellido2_Profesional, Telefono_Profesional, Descripcion;
+        bool Usuario_Premium, Perfil_Profesional;
+        
     }
 }
