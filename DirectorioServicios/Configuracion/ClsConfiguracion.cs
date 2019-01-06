9c588d47cd10cd16d4f34db90154d73b158ebb3b
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-06 16:24:36 -0600

    Varios

---
+++ DirectorioServicios/Configuracion/ClsConfiguracion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Configuracion
+{
+    public class ClsConfiguracion
+    {
+        public string getConnectionString
+        {
+            get
+            {
+                //se obtiene la cadena de conexion.
+                return ConfigurationManager.AppSettings["ConnectionString"];
+            }
+        }
+    }
+}
