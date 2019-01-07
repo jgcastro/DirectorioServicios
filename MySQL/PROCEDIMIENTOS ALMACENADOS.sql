c586761c6dbd8e4e03a16cf976276f45c5442199
Ariel Ramirez <arimonro@gmail.com>
2019-01-07 11:16:06 -0600

    Cambios a procedimientos MySQL
    new SP_ElimiarSitiosWeb

--- MySQL/PROCEDIMIENTOS ALMACENADOS.sql
+++ MySQL/PROCEDIMIENTOS ALMACENADOS.sql
@@ -1,11 +1,13 @@
--- ============== Proceso almacenado para buscar profesionales según su ocupación y ubicación ================ 
+
+USE DIRECTORIO_SERVICIOS;
+-- ============== Buscar profesionales según su ocupación y ubicación ================ --
+DELIMITER $
 -- ============== recibe como parámetros el nombre de la profesion,provincia y cantón y ordena la lista 
 -- ============== primero por el atributo premiun y luego por la calificación .
-
-use DIRECTORIO_SERVICIOS;
-
-DELIMITER $
-CREATE PROCEDURE  SP_ListaProfesionalesSolicitados(in _provincia varchar(20), in _canton varchar(20),in _ocupacion varchar(20))
+DROP PROCEDURE IF EXISTS `DIRECTORIO_SERVICIOS`.`SP_ListaProfesionalesSolicitados` $ -- facilita actualizar el procedimiento
+CREATE PROCEDURE `DIRECTORIO_SERVICIOS`.`SP_ListaProfesionalesSolicitados`(in _provincia varchar(20), 
+																			in _canton varchar(20),
+																			in _ocupacion varchar(20))
 BEGIN
 	SELECT 
 		usuarios.NOMBRE_PROFESIONAL,
@@ -26,71 +28,14 @@ BEGIN
 			order by usuarios.USUARIO_PREMIUM desc,usuarios.CALIFIC_SUMA desc;
 END $
 
-
 -- CALL SP_ListaProfesionalesSolicitados('San Jose','Escazu','JARDINERO')
 
+DELIMITER ;
 
-
-
-
--- Resive como parametro el id del usuario verifica si existe y elimina el registro del usuario 
--- verificando en cada tabla de la base de datos donde este el id del usuario que se va a eliminar
-
-
+-- ========== Agregar o Actualizar Pagina web ========== --
 DELIMITER $
-CREATE PROCEDURE SP_EliminarUsuario(in _id integer, out msj varchar(100))
-BEGIN
-    IF EXISTS(SELECT * FROM usuarios WHERE ID_USUARIO = _id)
-	THEN
-	BEGIN
-		  -- 1 eliminar sitios web
-		  IF EXISTS(SELECT * FROM websites WHERE ID_USUARIO = _id)
-		  THEN
-				DELETE FROM websites WHERE ID_USUARIO=_id;
-				
-            END IF;  
-             -- 2 eliminar ubicaciones
-            IF EXISTS(SELECT * FROM ubicaciones_profesionales WHERE ID_USUARIO = _id)
-			THEN
-				DELETE FROM ubicaciones_profesionales WHERE ID_USUARIO=_id;
-				
-            END IF;  
-             -- 3 eliminar ocupaciones
-			IF EXISTS(SELECT * FROM ocupaciones_profesionales WHERE ID_USUARIO = _id)
-			THEN
-				DELETE FROM ocupaciones_profesionales WHERE ID_USUARIO=_id;
-				
-            END IF;
-			-- 4 eliminar calificaciones
-			IF EXISTS(SELECT * FROM calificaciones WHERE ID_USUARIO = _id)
-			THEN
-				DELETE FROM calificaciones WHERE ID_USUARIO=_id;
-		
-            END IF;
-			-- 5 eliminar usuario
-			IF EXISTS(SELECT * FROM usuarios WHERE ID_USUARIO = _id)
-			THEN
-				DELETE FROM usuarios WHERE ID_USUARIO=_id;
-		
-            END IF;
-		  SET msj='Su cuenta fue eliminada con exito';
-	END;
-	ELSE
-		SET msj='no existe el usuario';
-	END IF;
-    
-	SELECT msj;
-END $
-
-
--- select msj='';
-
--- call SP_EliminarUsuario(3,@msj)
-
--- proceso almacenado para agregar paginas web 
-
-DELIMITER $
-CREATE PROCEDURE SP_AgregarSitiosWeb(in _cod_sitio int, in _id int ,in _url varchar(20),in _nombre varchar(20), out _msj varchar(100))
+DROP PROCEDURE IF EXISTS `DIRECTORIO_SERVICIOS`.`SP_AgregarSitioWeb` $ -- facilita actualizar el procedimiento
+CREATE PROCEDURE `DIRECTORIO_SERVICIOS`.`SP_AgregarSitioWeb`(in _cod_sitio int, in _id int ,in _url varchar(20),in _nombre varchar(20), out _msj varchar(100))
 BEGIN 
 	IF NOT EXISTS(SELECT * FROM websites WHERE COD_SITIO=_cod_sitio)
     THEN
@@ -109,3 +54,23 @@ BEGIN
     END IF;
 
 END $
+
+DELIMITER ;
+
+-- ========== Eliminar Pagina web ========== --
+DELIMITER $
+DROP PROCEDURE IF EXISTS `DIRECTORIO_SERVICIOS`.`SP_EliminarSitiosWeb` $ -- facilita actualizar el procedimiento
+CREATE PROCEDURE `DIRECTORIO_SERVICIOS`.`SP_EliminarSitiosWeb`(in _cod_sitio int, in _id int ,in _url varchar(20),in _nombre varchar(20), out _msj varchar(100))
+BEGIN 
+	IF EXISTS(SELECT * FROM websites WHERE COD_SITIO=_cod_sitio) THEN
+		BEGIN
+			DELETE FROM websites WHERE COD_SITIO=_cod_sitio;
+			SET _msj='El sitio web se elimino correctamente';
+		END; 
+    ELSE 
+		BEGIN 
+			SET _msj='El sitio web no existe';
+		END ;
+    END IF;
+END $
+DELIMITER ;
