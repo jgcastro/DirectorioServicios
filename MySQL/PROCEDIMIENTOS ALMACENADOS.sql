7639adff4ce65d087e21ee8bacf3c0557227db9a
Juan Castro <sempai211@gmail.com>
2019-01-08 14:11:01 -0600

    Corrección Ortográfica
    Se corrigieron algunos errores ortográficos

--- MySQL/PROCEDIMIENTOS ALMACENADOS.sql
+++ MySQL/PROCEDIMIENTOS ALMACENADOS.sql
@@ -22,17 +22,17 @@ BEGIN
         
     FROM usuarios inner join ocupaciones_profesionales on usuarios.ID_USUARIO=ocupaciones_profesionales.ID_USUARIO
 		inner join ocupaciones on ocupaciones.ID_OCUPACION=ocupaciones_profesionales.ID_OCUPACION
-		inner join ubicaciones_profesionales on usuarios.ID_USUARIO=ubicaciones_profecionales.ID_USUARIO
+		inner join ubicaciones_profesionales on usuarios.ID_USUARIO=ubicaciones_profesionales.ID_USUARIO
 		inner join ubicaciones on ubicaciones.ID_UBICAION=ubicaciones_profesionales.ID_UBICAION
 			where usuarios.PERFIL_PROFESIONAL=1 and  ubicaciones.PROVINCIA=_provincia and ubicaciones.CANTON=_canton and ocupaciones.NOMBRE_OCUPACION=_ocupacion
 			order by usuarios.USUARIO_PREMIUM desc,usuarios.CALIFIC_SUMA desc;
 END $
 
--- CALL SP_ListaProfesionalesSolicitados('San Jose','Escazu','JARDINERO')
+-- CALL SP_ListaProfesionalesSolicitados('San Jose','Escazú','JARDINERO')
 
 DELIMITER ;
 
--- ========== Agregar o Actualizar Pagina web ========== --
+-- ========== Agregar o Actualizar Página web ========== --
 DELIMITER $
 DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_AgregarSitioWeb` $ -- facilita actualizar el procedimiento
 CREATE PROCEDURE `PAGINA_WEB`.`SP_AgregarSitioWeb`(in _cod_sitio int, in _id int ,in _url varchar(20),in _nombre varchar(20), out _msj varchar(100))
@@ -41,7 +41,7 @@ BEGIN
     THEN
     BEGIN
 		INSERT INTO websites(ID_USUARIO,URL_SITIO,NOMBRE_SITIO) VALUES(_id,_url,_nombre);
-        SET _msj='El sitio web se agrego correctamente';
+        SET _msj='El sitio web se agregó correctamente';
         
     END; 
     ELSE 
@@ -49,7 +49,7 @@ BEGIN
 		UPDATE websites 
         SET URL_SITIO=_url,NOMBRE_SITIO=_nombre
         WHERE COD_SITIO =_cod_sitio;
-        SET _msj='El sitio web se actualizo correctamente';
+        SET _msj='El sitio web se actualizó correctamente';
     END ;
     END IF;
 
@@ -57,7 +57,7 @@ END $
 
 DELIMITER ;
 
--- ========== Eliminar Pagina web ========== --
+-- ========== Eliminar Página web ========== --
 DELIMITER $
 DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_EliminarSitiosWeb` $ -- facilita actualizar el procedimiento
 CREATE PROCEDURE `PAGINA_WEB`.`SP_EliminarSitiosWeb`(in _cod_sitio int, out _msj varchar(100))
@@ -65,7 +65,7 @@ BEGIN
 	IF EXISTS(SELECT * FROM websites WHERE COD_SITIO=_cod_sitio) THEN
 		BEGIN
 			DELETE FROM websites WHERE COD_SITIO=_cod_sitio;
-			SET _msj='El sitio web se elimino correctamente';
+			SET _msj='El sitio web se eliminó correctamente';
 		END; 
     ELSE 
 		BEGIN 
@@ -76,10 +76,10 @@ END $
 DELIMITER ;
 
 
--- ========== Agregar o Actualizar Ubicacion Profecional ========== --
+-- ========== Agregar o Actualizar Ubicación Profesional ========== --
 DELIMITER $
-DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_AgregarUbicacionProfecional` $ -- facilita actualizar el procedimiento
-CREATE PROCEDURE `PAGINA_WEB`.`SP_AgregarUbicacionProfecional`(in _ID_USUARIO int, 
+DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_AgregarUbicacionProfesional` $ -- facilita actualizar el procedimiento
+CREATE PROCEDURE `PAGINA_WEB`.`SP_AgregarUbicacionProfesional`(in _ID_USUARIO int, 
 																  in _ID_UBICACION int,
                                                                   in _DETALLES varchar(100),
                                                                   out _msj varchar(100))
@@ -88,7 +88,7 @@ BEGIN
     THEN
     BEGIN
 		INSERT INTO UBICACIONES_PROFESIONALES(ID_USUARIO,ID_UBICACION,DETALLES) VALUES(_ID_USUARIO,_ID_UBICACION,_DETALLES);
-        SET _msj='La ubicación se agrego correctamente';
+        SET _msj='La ubicación se agregó correctamente';
         
     END; 
     ELSE 
@@ -96,7 +96,7 @@ BEGIN
 		UPDATE UBICACIONES_PROFESIONALES 
         SET ID_UBICACION=_ID_UBICACION, DETALLES=_DETALLES
         WHERE ID_USUARIO=_ID_USUARIO AND ID_UBICACION=_ID_UBICACION;
-        SET _msj='La ubicacíon se actualizo correctamente';
+        SET _msj='La ubicacíon se actualizó correctamente';
     END ;
     END IF;
 
@@ -105,17 +105,17 @@ END $
 DELIMITER ;
 
 
--- =============== Eliminar Ubicacion Profecional ================= --
+-- =============== Eliminar Ubicación Profesional ================= --
 DELIMITER $
-DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_EliminarUbicacionProfecional` $ -- facilita actualizar el procedimiento
-CREATE PROCEDURE `PAGINA_WEB`.`SP_EliminarUbicacionProfecional`(in _ID_USUARIO int, 
+DROP PROCEDURE IF EXISTS `PAGINA_WEB`.`SP_EliminarUbicacionProfesional` $ -- facilita actualizar el procedimiento
+CREATE PROCEDURE `PAGINA_WEB`.`SP_EliminarUbicacionProfesional`(in _ID_USUARIO int, 
 																		  in _ID_UBICACION int,
 																		  out _msj varchar(100))
 BEGIN 
 	IF EXISTS(SELECT * FROM UBICACIONES_PROFESIONALES WHERE ID_USUARIO=_ID_USUARIO AND ID_UBICACION=_ID_UBICACION) THEN
 		BEGIN
 			DELETE FROM UBICACIONES_PROFESIONALES WHERE ID_USUARIO=_ID_USUARIO AND ID_UBICACION=_ID_UBICACION;
-			SET _msj='El sitio web se elimino correctamente';
+			SET _msj='El sitio web se eliminó correctamente';
 		END; 
     ELSE 
 		BEGIN 
