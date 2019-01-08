12bbc6002dde12f56060a0d2b5691559402c62d4
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-07 18:10:46 -0600

    Varios

---
+++ DirectorioServicios/DirectorioServicios/CapaLogica/LogicaUsuario.cs
@@ -0,0 +1,33 @@
+﻿using AccesoDatos;
+using EntidadesDirectorio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CapaLogica
+{
+    public class LogicaUsuario
+    {
+        
+
+        public DataTable listaUsuarios(string condition)
+        {
+            AccesoDatosUsuario accesoDatosUsuario = new AccesoDatosUsuario();
+            DataTable lista = null;
+            try
+            {
+                lista = accesoDatosUsuario.listaUsuarios(condition);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return lista;
+
+
+        }
+
+    }
+}
---
+++ DirectorioServicios/DirectorioServicios/Configuracion/ClsConfiguracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Configuracion
+{
+    public class ClsConfiguracion
+    {
+        public static string getConnectionString()
+        {
+            string conexion = "server=localhost;uid=root;pwd=1234;database=pagina_web;";
+            return conexion;
+        }
+    }
+}
